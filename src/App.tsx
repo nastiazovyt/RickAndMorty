1d62f3ee097e03543ccab315041ff534a9e81382
Anastasia Okisheva <nastiiazovyt@gmail.com>
2024-02-23 14:25:36 +0700

    - реализовала бесконечный скролл

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import {Character, getCharacters, ResultType} from "./shared/api";
 import {QueryClient, QueryClientProvider, useInfiniteQuery, useQuery} from "@tanstack/react-query";
 import {useEffect, useRef, useState} from "react";
 import {useClickAway} from "react-use";
+import {useInView} from "react-intersection-observer";
 
 const queryClient = new QueryClient()
 
@@ -18,18 +19,22 @@ function App() {
 function Characters() {
     const [isModalOpen, setIsModalOpen] = useState(false)
     const [activeCharacter, setActiveCharacter] = useState<ResultType>()
-    const [searchResult, setSearchResult] = useState('')
+    const [searchResult, setSearchResult] = useState<Element>()
+
+
+    const { ref, inView } = useInView({});
 
     const fetchData = async ({pageParam}: { pageParam: number }) => {
         return getCharacters(pageParam)
     }
 
-    const {data, status, error, fetchNextPage} = useInfiniteQuery({
+    const {data, status, error, fetchNextPage, hasNextPage, isFetchingNextPage} = useInfiniteQuery({
         queryKey: ['charactersData'],
         queryFn: fetchData,
         initialPageParam: 1,
         getNextPageParam: (lastPage, allPages) => {
-            return allPages.length + 1
+            const nextPage = lastPage.info.next ? allPages.length +1 : undefined
+            return nextPage
         }
     })
 
@@ -41,11 +46,17 @@ function Characters() {
             }}
                 className="hover:shadow-2xl transition-all ease-in-out duration-200 cursor-pointer rounded-2xl shadow-md overflow-hidden pb-8 flex flex-col gap-y-4"
                 key={character.id}>
-                <img src={character.image} alt={character.name}/>
+                <img className="w-80 h-auto" src={character.image} alt={character.name}/>
                 <span className="ps-3 font-raleway leading-5 text-green-700 text-md font-bold">{character.name}</span>
             </li>
         )
     })
+
+    useEffect(() => {
+        if (inView && hasNextPage) {
+            fetchNextPage()
+        }
+    }, [inView, hasNextPage, fetchNextPage]);
 
     if (status === 'pending') {
         return <p>Loading</p>
@@ -62,7 +73,7 @@ function Characters() {
             }}
                        className="hover:shadow-2xl transition-all ease-in-out duration-200 cursor-pointer rounded-2xl shadow-md overflow-hidden pb-8 flex flex-col gap-y-4"
                        key={results.id}>
-                <img src={results.image} alt={results.name}/>
+                <img className="w-80 h-auto" src={results.image} alt={results.name}/>
                 <span className="ps-3 font-raleway leading-5 text-green-700 text-md font-bold">{results.name}</span>
             </li>
         })
@@ -71,15 +82,16 @@ function Characters() {
     }
 
     return (
-        <div className="flex justify-center flex-col items-center pt-12">
-            <h1 className="pt-6 mb-12 font-raleway leading-3 text-green-900 text-5xl font-extrabold">Characters</h1>
+        <div className="flex justify-center flex-col items-center mb-6">
+            <h1 className="pt-6 mb-8 font-raleway text-green-900 text-5xl font-extrabold">Characters</h1>
             <div className="grid grid-flow-col gap-6">
                 <div className="row-span-3 w-72 border-2 h-96">Фильтры</div>
                 <SearchBox onChange={changeSearchData}/>
-                <ul>{searchResult}</ul>
-                <ul className="grid grid-cols-3 gap-x-6 gap-y-12 w-fit m-auto row-span-2 col-span-2 mb-6">{characters}</ul>
+                <div className="flex items-center flex-col row-span-2 col-span-2">
+                    <ul className="grid grid-cols-3 gap-x-6 gap-y-8 w-fit m-auto  mb-6">{!searchResult ? characters : searchResult}</ul>
+                    <span className="font-raleway" ref={ref}>{!hasNextPage ? 'nothing to load, this is the end =(' : isFetchingNextPage ? 'loading data...' : ''}</span>
+                </div>
             </div>
-            <button onClick={() => fetchNextPage()}>Load more</button>
             {isModalOpen && activeCharacter && <Modal modalCloser={setIsModalOpen} modalContent={activeCharacter}/>}
         </div>
     )
@@ -123,21 +135,41 @@ function Modal({modalContent, modalCloser}: { modalContent: ResultType; modalClo
                         </div>
                         <div>
                             <span className="text-2xl font-bold text-green-950 block mb-3">Episodes:</span>
-                            <ul className="flex flex-col gap-y-1.5">
-                                <li>здесь будут вводиться ссылки с названиями эпизодов</li>
-                                <li>здесь будут вводиться ссылки с названиями эпизодов</li>
-                                <li>здесь будут вводиться ссылки с названиями эпизодов</li>
-                                <li>здесь будут вводиться ссылки с названиями эпизодов</li>
+                            <ul className="flex flex-col gap-y-1.5 h-52 overflow-auto">
+                                <li>
+                                    <span className="line-clamp-1">здесь будут вводиться ссылки с названиями эпизодов</span>
+                                </li>
+                                <li>
+                                    <span className="line-clamp-1">здесь будут вводиться ссылки с названиями эпизодов</span>
+                                </li>
+                                <li>
+                                    <span className="line-clamp-1">здесь будут вводиться ссылки с названиями эпизодов</span>
+                                </li>
+                                <li>
+                                    <span className="line-clamp-1">здесь будут вводиться ссылки с названиями эпизодов</span>
+                                </li>
+                                <li>
+                                    <span className="line-clamp-1">здесь будут вводиться ссылки с названиями эпизодов</span>
+                                </li>
+                                <li>
+                                    <span className="line-clamp-1">здесь будут вводиться ссылки с названиями эпизодов</span>
+                                </li>
+                                <li>
+                                    <span className="line-clamp-1">здесь будут вводиться ссылки с названиями эпизодов</span>
+                                </li>
+                                <li>
+                                    <span className="line-clamp-1">здесь будут вводиться ссылки с названиями эпизодов</span>
+                                </li>
                             </ul>
                         </div>
                         <div className="">
                             <span className="text-2xl font-bold text-green-950 mb-3 block">Locations:</span>
-                            <ul className="flex flex-col gap-y-1.5">
-                                <li className="flex gap-x-3 text-lg">
+                            <ul className="flex flex-col gap-y-6">
+                                <li className="flex flex-col text-lg">
                                     <span className="text-green-900 font-bold">last known location:</span>
                                     <span>{modalContent.location.name}</span>
                                 </li>
-                                <li className="flex gap-x-3 text-lg">
+                                <li className="flex flex-col text-lg">
                                     <span className="text-green-900 font-bold">origin location:</span>
                                     <span>{modalContent.origin.name}</span>
                                 </li>
@@ -172,7 +204,7 @@ function SearchBox({onChange}) {
 
     return (
         <div className="col-span-2 h-12 border-2 p-2">
-            <input className="focus:outline-none w-full" type="search" placeholder="Enter your search term here"
+            <input className="focus:outline-none w-full font-raleway" type="search" placeholder="Enter your search term here"
                    value={search} onChange={(e) => {
                 setSearch(e.target.value);
                 changeSearchData(data)
